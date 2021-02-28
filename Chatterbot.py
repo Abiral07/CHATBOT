4f673fa79e4d34d024b0299d71e4ea3d5b3cf18d
Abiral07 <abiralkhadka7@gmail.com>
2021-02-28 20:59:15 +0545

    Added loss and learning rate visulization using matplotliv and added invoke send on pressing enter

--- Chatterbot.py
+++ Chatterbot.py
@@ -29,18 +29,21 @@ model.eval()
 
 #create chat
 from tkinter import *
+from tkinter.scrolledtext import *
+
 root = Tk()
 # w, h = root.winfo_screenwidth(), root.winfo_screenheight()
 # root.geometry("%dx%d+0+0" % (w, 670))
 root.geometry("900x600+10+10")
 # root.attributes('-zoomed', True)
 bot_name="BOT"
-def send():
+def ask(event=None):
         sentence = e.get() 
         txt.insert(END,"\n"+"YOU: "+sentence)
         sentence = sentence.lower()
         if sentence =="bye" :
-            txt.insert(END,"\n"+bot_name +": Visit Again :)"+"\n")
+            ans=" Visit Again"
+            txt.insert(END,"\n"+bot_name +":"+ans+"\n")
             time.sleep(1)
             root.quit()
         else:
@@ -68,13 +71,17 @@ def send():
                         txt.insert(END,"\n"+ bot_name +": "+ response +"\n")
             else:
                 txt.insert(END,"\n"+bot_name+": Sorry! I did'nt understand your problem."+"\n")
+        txt.yview(END)
         e.delete(0,END)
+        return 0
 
-txt = Text(root, width=170, height=35)
-txt.grid(row=2,column=0,columnspan=2)
+
+txt = ScrolledText(root, width=110,  height=35, borderwidth=1)
+txt.grid(row=0,column=0,columnspan=2)
 # txt.insert(END,"Hello there! I'm here to help you in case you need any First-Aid advice. Let's chat! (type 'bye' to exit)"+"\n")
-e= Entry(root, width=200,bg="#9D00FF", fg="white", borderwidth=3)
-send=Button(root,text="Send", command=send, width=20, bg="#24a0ed", fg="white").grid(row=0,column=0, rowspan=2) 
-e.grid(row=0,column=1, rowspan=2)
+e= Entry(root,bg="#9D00FF", width=75,font=("Verdana"), fg="white", borderwidth=1)
+sendbtn = Button(root,text="Send", command=ask, width=10, font=("Verdana"), bg="#24a0ed", fg="white").grid(row=1,column=1, rowspan=2) 
+e.grid(row=1,column=0, rowspan=2)
 root.title("CHATBOT")
+root.bind('<Return>', ask)
 root.mainloop()
--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 import json
 import numpy as np
-
+import matplotlib.pyplot as plt
+# % matplotlib inline
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset,DataLoader
-
 
 from model import FFNeuralNet
 with open('intents.json', 'r',encoding='cp437') as f:
@@ -37,7 +37,6 @@ all_words = [stem(w) for w in all_words if w not in ignore_words]
 # remove duplicates and sort
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
-
 # print(all_words)
 # print(tags)
 # print(xy)
@@ -79,17 +78,14 @@ batch_size = 8
 input_size = len(all_words)#len(X_train[0])length of each bow we created same len as allwords
 hidden_size = 94
 output_size = len(tags)#NO. OF DIFFERENT CLASSES OR TAGS
-learning_rate=0.001
-num_epochs= 100
+learning_rate=0.01
+num_epochs= 70
 # print(input_size, len(all_words))
 # print(output_size, tags)
 
     
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                         )
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 # pylint: disable=E1101
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # pylint: enable=E1101
@@ -100,29 +96,49 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 #Specify learning rate in hperparameters
 
 # Train the model
-for epoch in range(num_epochs):
-    for (words, labels) in train_loader:
-        words = words.to(device)
-        # pylint: disable=E1101
-        labels = labels.to(dtype=torch.long).to(device)
-        # pylint: enable=E1101
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
 
-        # Forward pass
-        outputs = model(words)
-        loss = criterion(outputs, labels)
+def fit(epochs,max_lr,model,device,train_loder,opt_func,loss_func):
+    history=[]
+    # Set up one-cycle learning rate scheduler
+    sched = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr, epochs=epochs, 
+                                                steps_per_epoch=len(train_loader))
+    for epoch in range(epochs):
+        lrs=[]
+        for (words, labels) in train_loader:
+            words = words.to(device)
+            # pylint: disable=E1101
+            labels = labels.to(dtype=torch.long).to(device)
+            # pylint: enable=E1101
+
+            # Forward pass
+            outputs = model(words)
+            loss = loss_func(outputs, labels)
+            
+            # Backward and optimize
+            loss.backward()
+            optimizer.step()
+            opt_func.zero_grad()
+            # Record & update learning rate
+            current_lr = get_lr(optimizer)
+            lrs.append(current_lr)
+            sched.step()
         
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+ 
+        history.append({'loss':loss.item(),'lrs':lrs})
+        # print(history)
+        if (epoch+1) % 10 == 0:
+            #in every 10stp we print current epoch and all epoch
+            print (f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}, lr:{current_lr}')
+            
+    print(f'final loss: {loss.item():.4f}')
+    return history
+
         
-        
-    if (epoch+1) % 10 == 0:
-        #in every 100stp we print current epoch and all epoch
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
-        print(f'final loss: {loss.item():.4f}')
-        
-        
+history = fit(num_epochs,learning_rate,model,device,train_loader,optimizer,criterion)
+
 #save data        
 data = {
 "model_state": model.state_dict(),
@@ -135,6 +151,25 @@ data = {
 #serialize and save it to file
 FILE = "data.pth"
 torch.save(data, FILE)
-
 print(f'training complete. file saved to {FILE}')
 
+def plot_losses(history):
+    train_losses = [x['loss'] for x in history]
+    print (train_loader)
+    plt.plot(train_losses,'-x')
+    plt.xlabel('Epoch')
+    plt.ylabel('Losses')
+    plt.title('LOSS VS EPOCH')
+    plt.show()
+
+
+def plot_lrs(history):
+    lrs = np.concatenate([x.get('lrs',[]) for x in history])
+    plt.plot(lrs,'-x')
+    plt.xlabel('Batch no.')
+    plt.ylabel('Learning rate')
+    plt.title('Learning rate VS Batch no.')
+    plt.show()
+
+plot_losses(history)
+plot_lrs(history)
